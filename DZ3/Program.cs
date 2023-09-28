bd971ab61b7865bfe203d4660b15f38996539ad9
user <user@WIN-EL4IOEQBAEK>
2023-09-28 21:13:17 +0300

    Добавьте файлы проекта.

---
+++ DZ3/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ3
+{
+    internal class Program
+    {
+        enum Weekdays
+        {
+            Понедельник = 1,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 2.(Игральным картам условно присвоены порядковые номера в зависимости от их достоинства,определить достоинство соответствующей карты):");
+            try
+            {
+                Console.WriteLine("Введите номер карты от 6 до 14(после ввода нажмите enter): ");
+                int cardNumber = Convert.ToInt32(Console.ReadLine());
+                switch (cardNumber)
+                {
+                    case 6:
+                        Console.WriteLine("шестерка");
+                        break;
+                    case 7:
+                        Console.WriteLine("семёрка");
+                        break;
+                    case 8:
+                        Console.WriteLine("восьмерка");
+                        break;
+                    case 9:
+                        Console.WriteLine("девятка");
+                        break;
+                    case 10:
+                        Console.WriteLine("десятка");
+                        break;
+                    case 11:
+                        Console.WriteLine("валет");
+                        break;
+                    case 12:
+                        Console.WriteLine("дама");
+                        break;
+                    case 13:
+                        Console.WriteLine("король");
+                        break;
+                    case 14:
+                        Console.WriteLine("туз");
+                        break;
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Задание 3.(Напишите программу, которая принимает на входе строку и производит выходные данные) ");
+            Console.WriteLine("Введите сообщение(после ввода нажмите enter): ");
+            string Message = Console.ReadLine().ToLower();
+            switch (Message)
+            {
+                case "jabroni":
+                    Console.WriteLine("Patron Tequila");
+                    break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol");
+                    break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer");
+                    break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine");
+                    break;
+                case "politician":
+                    Console.WriteLine("Your tax dollars");
+                    break;
+                case "rapper":
+                    Console.WriteLine("Cristal");
+                    break;
+                default:
+                    Console.WriteLine("Beer");
+                    break;
+            }
+
+            Console.WriteLine("Задание 4.(составить программу, которая в зависимости от порядкового номера дня недели (1, 2,...,7) выводит на экран его название (понедельник, вторник, ..., воскресенье))");
+            Console.WriteLine("Введите порядковый номер от 1 до 7(после ввода нажмите enter):");
+            int numberofday = int.Parse(Console.ReadLine());
+            if (numberofday <= 7 && numberofday >= 1)
+            {
+                Weekdays dayofweek = (Weekdays)numberofday;
+                Console.WriteLine("День недели: " + dayofweek);
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введен порядковый номер дня недели!");
+            }
+            
+            Console.WriteLine("Задание 5.(нужно создать массив и обойти его, собрав нужные элементы в 'сумку')");
+            string[] toys = { "Hello Kitty", "Teddy bear", "Dinosaur", "Hello Kitty", "Book", "Car", "Barbie doll" };
+            int dollsCount = 0;
+
+            foreach (string toy in toys)
+            {
+                if (toy == "Hello Kitty" || toy == "Barbie doll")
+                {
+                    dollsCount++;
+                }
+            }
+
+            Console.WriteLine("В сумке " + dollsCount + " кукол");
+
+
+        }
+    }
+}
---
+++ Tumakov_Lab4/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumakov_Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 4.1, 4.2 и домашнее задание 4.1(нужно считать с экрана число от 1 до 365, перевести этот число в месяц и день месяца, а также определить год високосный или нет)");
+            Console.WriteLine("Введите год(после ввода нажмите enter): ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите номер дня в году(после ввода нажмите enter): ");
+            int dayOfYear = int.Parse(Console.ReadLine());
+            int month = 0;
+            int dayOfMonth = dayOfYear;
+            bool isLeapYear = IsLeapYear(year);
+            if (isLeapYear)
+            {
+                if (dayOfMonth > 366 || dayOfMonth < 1)
+                {
+                    Console.WriteLine("Неверно введён день!");
+                }
+                else if (dayOfMonth <= 31)
+                {
+                    month = 1;
+                }
+                else if (dayOfMonth <= 60)
+                {
+                    month = 2;
+                    dayOfMonth -= 31;
+                }
+                else if (dayOfMonth <= 91)
+                {
+                    month = 3;
+                    dayOfMonth -= 60;
+                }
+                else if (dayOfMonth <= 121)
+                {
+                    month = 4;
+                    dayOfMonth -= 91;
+                }
+                else if (dayOfMonth <= 152)
+                {
+                    month = 5;
+                    dayOfMonth -= 121;
+                }
+                else if (dayOfMonth <= 182)
+                {
+                    month = 6;
+                    dayOfMonth -= 152;
+                }
+                else if (dayOfMonth <= 213)
+                {
+                    month = 7;
+                    dayOfMonth -= 182;
+                }
+                else if (dayOfMonth <= 244)
+                {
+                    month = 8;
+                    dayOfMonth -= 213;
+                }
+                else if (dayOfMonth <= 274)
+                {
+                    month = 9;
+                    dayOfMonth -= 244;
+                }
+                else if (dayOfMonth <= 305)
+                {
+                    month = 10;
+                    dayOfMonth -= 274;
+                }
+                else if (dayOfMonth <= 335)
+                {
+                    month = 11;
+                    dayOfMonth -= 305;
+                }
+                else if (dayOfMonth <= 366)
+                {
+                    month = 12;
+                    dayOfMonth -= 335;
+                }
+            }
+            else
+            {
+                if (dayOfMonth > 365 || dayOfMonth < 1)
+                {
+                    Console.WriteLine("Неверно введён день!");
+                }
+                else if (dayOfMonth <= 31)
+                {
+                    month = 1;
+                }
+                else if (dayOfMonth <= 59)
+                {
+                    month = 2;
+                    dayOfMonth -= 31;
+                }
+                else if (dayOfMonth <= 90)
+                {
+                    month = 3;
+                    dayOfMonth -= 59;
+                }
+                else if (dayOfMonth <= 120)
+                {
+                    month = 4;
+                    dayOfMonth -= 90;
+                }
+                else if (dayOfMonth <= 151)
+                {
+                    month = 5;
+                    dayOfMonth -= 120;
+                }
+                else if (dayOfMonth <= 181)
+                {
+                    month = 6;
+                    dayOfMonth -= 151;
+                }
+                else if (dayOfMonth <= 212)
+                {
+                    month = 7;
+                    dayOfMonth -= 181;
+                }
+                else if (dayOfMonth <= 243)
+                {
+                    month = 8;
+                    dayOfMonth -= 212;
+                }
+                else if (dayOfMonth <= 273)
+                {
+                    month = 9;
+                    dayOfMonth -= 243;
+                }
+                else if (dayOfMonth <= 304)
+                {
+                    month = 10;
+                    dayOfMonth -= 273;
+                }
+                else if (dayOfMonth <= 334)
+                {
+                    month = 11;
+                    dayOfMonth -= 304;
+                }
+                else if (dayOfMonth <= 365)
+                {
+                    month = 12;
+                    dayOfMonth -= 334;
+                }
+            }
+
+            Console.WriteLine("Дата: " + dayOfMonth + " " + GetMonthName(month));
+        }
+
+        static string GetMonthName(int month)
+        {
+            switch (month)
+            {
+                case 1:
+                    return "января";
+                case 2:
+                    return "февраля";
+                case 3:
+                    return "марта";
+                case 4:
+                    return "апреля";
+                case 5:
+                    return "мая";
+                case 6:
+                    return "июня";
+                case 7:
+                    return "июля";
+                case 8:
+                    return "августа";
+                case 9:
+                    return "сентября";
+                case 10:
+                    return "октября";
+                case 11:
+                    return "ноября";
+                case 12:
+                    return "декабря";
+                default:
+                    return "";
+            }
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
